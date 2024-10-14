670fc7e2a5f1752222e755256a06e1f8748fed95
Bhuwan-Upadhyay <bhuwanb898@gmail.com>
2024-10-14 14:08:24 +0530

    fix bugs

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -27,22 +27,23 @@ const Player = ({ audioScr, songCover, isDisplay, setIsDisplay }) => {
 
   return (
     <div
-      className={`bg-gray-400 border border-red-500 h-[350px] w-[400px] rounded-md overflow-hidden absolute top-10 ${
-        isDisplay ? "flex" : "hidden"
-      } transition-transform duration-500 flex-col`}
-    >
-      <img className="h-[70%] w-full object-cover" src={songCover} alt="" />
+  className={`bg-gray-400 border border-red-500 h-[350px] w-[400px] rounded-md overflow-hidden fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 ${
+    isDisplay ? "flex" : "hidden"
+  } transition-transform duration-500 flex-col`}
+>
+  <img className="h-[70%] w-full object-cover" src={songCover} alt="" />
 
-      <div className="btns flex justify-center">
-        <button className="p-1" onClick={handleAudio}>
-          {currSongPlaying ? <FaPause /> : <FaPlay />}
-        </button>
-      </div>
-      <audio ref={AudioRef} src={audioScr}></audio>
-      <button onClick={handleClose} className="bg-red-600">
-        Close
-      </button>
-    </div>
+  <div className="btns flex justify-center">
+    <button className="p-1" onClick={handleAudio}>
+      {currSongPlaying ? <FaPause /> : <FaPlay />}
+    </button>
+  </div>
+  <audio ref={AudioRef} src={audioScr}></audio>
+  <button onClick={handleClose} className="bg-red-600">
+    Close
+  </button>
+</div>
+
   );
 };
 
